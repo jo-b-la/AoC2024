f3c666c5429ce7170b33045a59586e0c537a1079
Johann Blauensteiner <johann.blauensteiner@acl.at>
2024-12-04 08:20:59 +0100

    Fix dx, dy, Day 4_2

--- Day_4/solution_4_2.py
+++ Day_4/solution_4_2.py
@@ -16,11 +16,8 @@ def read_grid_from_file(filename):
     with open(filename, 'r') as file:
         return [list(line.strip()) for line in file]
 
-def check_for_word(grid, rows,cols,row,col,dx,dy,word):
-    if not (0 <= row + (len(word)-1)*dy < rows and
-            0 <= col + (len(word)-1)*dx < cols):
-           return False
-
+def check_for_word(grid, row,col,dx,dy,word):
+    # print(row,col,dx,dy,word)
     for i in range(len(word)):
            if grid[row + i*dy][col + i*dx] != word[i]:
                return False
@@ -37,10 +34,12 @@ def process_file(filename,word):
 
     total_xmas=0
 
-    for row in range(rows):
-       for col in range(cols):
-           for dx, dy in directions:
-                if check_for_word(grid, rows,cols,row,col,dx,dy,word):
+    for row in range(rows-word_len-1):
+       for col in range(cols-word_len-1):
+            # Top Down Check
+            if check_for_word(grid,row,col,1,1,word) or check_for_word(grid,row,col,1,1,word[::-1]):
+                # Bottom up check
+                if check_for_word(grid,row+word_len-1,col,1,-1,word) or check_for_word(grid,row+word_len-1,col,1,-1,word[::-1]):
                     total_xmas += 1
     print (total_xmas)
 
@@ -48,7 +47,7 @@ def main():
     filename = "input.txt"
     current_dir = Path(__file__).parent
     input_file = current_dir / filename
-    process_file(input_file, "XMAS")
+    process_file(input_file, "MAS")
 
 
 if __name__ == "__main__":
